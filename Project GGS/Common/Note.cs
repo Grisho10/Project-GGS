f7f5543bc22435bf224fd5e7187be9013378e3b6
PC <PC@DESKTOP-9ALBT8S>
2023-03-11 14:39:20 +0200

    Add project files.

---
+++ Project GGS/Common/Note.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_GGS
+{
+    class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Level  { get; set; }
+
+        public Note()
+        {
+        }
+        public Note(int id, string title, string description, int level)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Description = description;
+            this.Level = level;
+        }
+
+    }
+}
